2943fd74aea5f0496ccad8a7c156a3081ae2a671
Karthick <itkarthi02@gmail.com>
2016-01-04 07:51:45 +0530

    KNN Classifier

--- seeds.py
+++ seeds.py
@@ -21,4 +21,30 @@ print (features[:6])
 print (labels.shape)
 print (labels[:6])
 
+#KNN Model
+
+def learn_model(k,features,labels):
+  return k,features.copy(),labels.copy()
+
+
+#Finding the Class of max nieghbors ( for KNN )
+def plurality(xs):
+  from collections import defaultdict
+  counts = defaultdict(int)
+  for x in xs:
+    counts[x] += 1
+  maxv = max(counts.values())
+  for k,v in counts.items():
+    if v == maxv:
+      return k
+
+
+
+def apply_model(features,model):
+  k,train_feats,labels = model  
+  results = []
+  for f in features:
+    label_dist = []
+    for t,ell in zip(train_feats,labels):
+      label_dist.append( (np.linalg.norm(f-t), ell) )
 
